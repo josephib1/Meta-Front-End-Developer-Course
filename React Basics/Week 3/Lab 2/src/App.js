97ef50390f3566d1d559299f443bd90c1ea4eda3
josephib1 <joseph.ibrahim718@gmail.com>
2023-10-26 16:26:59 +0300

    Week 3

---
+++ React Basics/Week 3/Lab 2/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./assets/logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task: Add an image below</h1>
+      <img src={logo} alt="logo"/>
+
+    </div>
+  );
+}
+
+export default App;
